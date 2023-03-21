4c08633959c8dd34bf4d7b4419f8b5f9fe29819b
zainadomi <92091835+zainadomi@users.noreply.github.com>
2023-03-21 12:34:59 +0300

    installation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { React, useState } from "react";
 import { Home } from "./Home";
+import { Login } from "./Components/Login";
+import { Register } from "./Components/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GetSearchData } from "./Components/GetSearchData";
 import MovieDetails from "./Components/MovieDetails";
@@ -11,19 +13,29 @@ import store from './redux/store';
 
 
 function App() {
-  
-  const [query, setQuery] = useState();
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home setQuery={setQuery} />} />
-          <Route path="/search" element={<GetSearchData query={query} />} />
-          <Route path="/details/:movieId" element={<MovieDetails />} />
-          <Route path="watchList/:movieId" element={<WatchList query={query} />} />
 
-        </Routes>
-      </BrowserRouter></Provider>
+  const [currentForm,setCurrentForm] = useState('login');
+  const [query, setQuery] = useState();
+
+  const toggleForm = (forName)=>{
+    setCurrentForm(forName);
+  }
+  return (
+    <div className="App">
+     {
+      currentForm == "login" ?<Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>  
+     } 
+    </div>
+    // <Provider store={store}>
+    //   <BrowserRouter>
+    //     <Routes>
+    //       <Route path="/" element={<Home setQuery={setQuery} />} />
+    //       <Route path="/search" element={<GetSearchData query={query} />} />
+    //       <Route path="/details/:movieId" element={<MovieDetails />} />
+    //       <Route path="watchList/:movieId" element={<WatchList query={query} />} />
+
+    //     </Routes>
+    //   </BrowserRouter></Provider>
 
 
   )
---
+++ src/Components/Login.js
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+export const Login = (props) => {
+
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+
+    return (
+        <div className="auth-form-container">
+            <h2>Login</h2>
+
+            <form className="login-form" onSubmit={handleSubmit}>
+
+                <label htmlFor="email">email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@email.com" id="email" name="email" />
+
+                <label htmlFor="password">password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="password" id="password" name="password" />
+
+                <button className="auth-btn">Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
+
+    )
+}
